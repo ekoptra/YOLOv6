5114cb3115000e7111330d63ba14dcb43dc56003
Eko Putra <52368104+ekoptra@users.noreply.github.com>
2023-08-27 07:07:48 +0000

    again

--- yolov6/models/losses/loss.py
+++ yolov6/models/losses/loss.py
@@ -181,7 +181,7 @@ class ComputeLoss:
         for i, item in enumerate(targets.cpu().numpy().tolist()):
             targets_list[int(item[0])].append(item[1:])
         max_len = max((len(l) for l in targets_list))
-        targets = torch.from_numpy(np.array(list(map(lambda l:l + [[-1,0,0,0,0]]*(max_len - len(l)), targets_list)))[:,1:,:]).to(targets.device)
+        targets = torch.from_numpy(np.array(list(map(lambda l:l + [[-1,0,0,0,0]]*(max_len - len(l)), targets_list)))[:,1:,:])
         batch_target = targets[:, :, 1:5].mul_(scale_tensor)
         targets[..., 1:] = xywh2xyxy(batch_target)
         return targets
